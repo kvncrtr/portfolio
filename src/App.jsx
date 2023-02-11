e6e543ba0ff40c85e6930663924b511ddace6bbe
Kevin Carter <console.logkevin@gmail.com>
2023-02-11 10:08:21 -0500

    navbar feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
+
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <Navbar />
     </div>
   )
 }
