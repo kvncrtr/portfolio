07255060b524f89667d4f186de90b8a41af4f764
Kevin Carter <console.logkevin@gmail.com>
2023-02-12 08:28:08 -0500

    add about menu option and package-lock.json file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,21 +2,22 @@ import { useRef } from "react"
 import { FaBars, FaTimes } from 'react-icons/fa'
 
 export default function Navbar() {
-   const navRef = useRef()
+   const navRef = useRef(null)
 
    const showNavbar = () => {
-      navRef.current.classList.toggle('responsive_nav')
+      navRef.current.classList.toggle('responsive--nav')
    }
 
    return (
       <div className='navbar--container'>
          <h3>Kevin Carter</h3>
          <nav ref={navRef}>
-            <a href='Home'>Home</a>
-            <a href='Skills'>Skills</a>
-            <a href='Languages'>Languages</a>
-            <a href='Projects'>Projects</a>
-            <a href='Contact'>Contact</a>
+            <a href='/#'>Home</a>
+            <a href='/#'>Skills</a>
+            <a href='/#'>Languages</a>
+            <a href='/#'>Projects</a>
+            <a href='/#'>About</a>
+            <a href='/#'>Contact</a>
             <button onClick={showNavbar}>
                <FaTimes />
             </button>
