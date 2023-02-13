1c83f9857a8d0952ffc1ce0c2bad334fc357a791
Kevin Carter <console.logkevin@gmail.com>
2023-02-13 12:42:06 -0500

    add hero section without minor 3d colored shape behind image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,16 +4,18 @@ import headshot from '../assets/headshot.jpg'
 export default function Hero() {
    return (
       <div className={'hero--container'}>
+
          <div className={'hero--landing'}>
             <img className={'hero--headshot'} src={headshot} alt="headshot" />
             <div className={'hero--img-shape'}></div>
-            
-            <p></p>
-            <h1></h1>
-            <h1></h1>
-            <h1></h1>
+
+            <p className={'hero--intro'}>Hello, my name is</p>
+            <h1 className={'hero--name'}>Kevin Carter</h1>
+            <h1 className={'hero--skill'}>Front-End Developer</h1>
+            <h1 className={'hero--location'}>Atlanta, Ga</h1>
             <div className={'hero--bottom-shape'}></div>
          </div>
+
       </div>
    )
 } 
