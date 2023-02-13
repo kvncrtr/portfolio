b35de96414d4935ef0cc8c050f1fa27495d38ebf
Kevin Carter <console.logkevin@gmail.com>
2023-02-13 10:20:47 -0500

    layout html for hero landing section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,16 @@ import headshot from '../assets/headshot.jpg'
 export default function Hero() {
    return (
       <div className={'hero--container'}>
-         <img className={'hero--headshot'} src={headshot} alt="headshot" />
+         <div className={'hero--landing'}>
+            <img className={'hero--headshot'} src={headshot} alt="headshot" />
+            <div className={'hero--img-shape'}></div>
+            
+            <p></p>
+            <h1></h1>
+            <h1></h1>
+            <h1></h1>
+            <div className={'hero--bottom-shape'}></div>
+         </div>
       </div>
    )
 } 
