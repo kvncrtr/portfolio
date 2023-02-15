8184bb207cda466e28e43ea60f93be0cf449dc77
Kevin Carter <console.logkevin@gmail.com>
2023-02-15 09:29:09 -0500

    skill section at a working status

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Skill from './components/Skill'
-import Languages from './components/Languages'
 import './styles/app.css'
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
       <Navbar />
       <Hero />
       <Skill />
-      <Languages />
     </div>
   )
 }
