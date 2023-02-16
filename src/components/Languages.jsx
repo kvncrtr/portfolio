de5d41810dcf5e0df624fa07f7f3e7473ed0324f
Kevin Carter <console.logkevin@gmail.com>
2023-02-15 21:25:27 -0500

    lang section done

--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -9,21 +9,55 @@ import bootstrap from '../assets/logos/bootstrap-logo.png'
 import jquery from '../assets/logos/jquery-logo.png'
 import sass from '../assets/logos/sass-logo.png'
 
+import image from '../assets/pexels-steve-johnson-1843717.jpg'
+
 export default function Languages() {
    return (
       <div className={'lang--container'}>
 
-         {/* <h1 className={'lang--header'}>Tech I've Used</h1> */}
+         <h1 className={'lang--header'}>Tech I've Used</h1>
+         <img className={'lang--bkrnd'} src={image} alt="white texture" />
 
          <div className={'lang--logo-case'}>
-            <img className={'lang--logo'} src={html} alt="html logo" />
-            <img className={'lang--logo'} src={css} alt="css logo" />
-            <img className={'lang--logo'} src={javascript} alt="javascript logo" />
-            <img className={'lang--logo'} src={react} alt="react logo" />
-            <img className={'lang--logo'} src={redux} alt="redux logo" />
-            <img className={'lang--logo'} src={bootstrap} alt="bootstrap logo" />
-            <img className={'lang--logo'} src={jquery} alt="jquery logo" />
-            <img className={'lang--logo'} src={sass} alt="sass logo" />
+            <div className={'lang--logo-box'}>
+               <h3>HTML</h3>
+               <img className={'lang--logo'} src={html} alt="html logo" />
+            </div>
+
+            <div className={'lang--logo-box'}>
+               <h3>CSS</h3>
+               <img className={'lang--logo'} src={css} alt="css logo" />
+            </div>
+
+            <div className={'lang--logo-box'}>
+               <h3>JavaScript</h3>
+               <img className={'lang--logo'} src={javascript} alt="javascript logo" />
+            </div>
+
+            <div className={'lang--logo-box'}>
+               <h3>React</h3>
+               <img className={'lang--logo'} src={react} alt="react logo" />
+            </div>
+
+            <div className={'lang--logo-box'}>
+               <h3>Redux</h3>
+               <img className={'lang--logo'} src={redux} alt="redux logo" />
+            </div>
+
+            <div className={'lang--logo-box'}>
+               <h3>BootStrap</h3>
+               <img className={'lang--logo'} src={bootstrap} alt="bootstrap logo" />
+            </div>
+
+            <div className={'lang--logo-box'}>
+               <h3>jQuery</h3>
+               <img className={'lang--logo'} src={jquery} alt="jquery logo" />
+            </div>
+
+            <div className={'lang--logo-box'}>
+               <h3>SASS</h3>
+               <img className={'lang--logo'} src={sass} alt="sass logo" />
+            </div>
          </div>
 
       </div>
