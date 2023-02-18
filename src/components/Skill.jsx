2d2788aa3f094763d527ae074c68a505c95ff1d0
Kevin Carter <console.logkevin@gmail.com>
2023-02-18 07:37:24 -0500

    remove third skill

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -23,13 +23,6 @@ export default function Skill() {
                </p>
             </div>
 
-            <div className={'skill--framework'}>
-               <h3 className={'section--subtitle skill--card-title'}>FRONT-END</h3>
-               <p className={'skill--description'}>
-                  There is therefore now no condemnation for those who are in Christ Jesus.[a] 2 For the law of the Spirit of life has set you[b] free in Christ Jesus from the law of sin and death. 3 For God has done what the law, weakened by the flesh, could not do. By sending his own Son in the likeness of sinful flesh and for sin,[c] he condemned sin in the flesh, 4 in order that the righteous requirement of the law might be fulfilled in us, who walk not according to the flesh but according to the Spirit.
-               </p>
-            </div>
-
          <img className={'skill--img img'} src={skillPicture} alt={'picture of vs code'} />
          <img className={'skill--img2 img'} src={skillPicture2} alt={'picture of vs code'} />
         
