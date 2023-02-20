ca591fa350fa89ecd2d0c4d6f6bff85bb0cebd3c
Kevin Carter <console.logkevin@gmail.com>
2023-02-20 08:57:28 -0500

    move projects section to it's own component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,94 @@
+import React from 'react'
+
+export default function Projects() {
+   return (
+      <section className={'section'}>
+         <div className={'project--container'}>
+
+            <div className={'project--card'}>
+
+               <img src='' alt='' />
+
+               <div className={'project--specs'}>              
+                  <p>Front-End Developer</p>
+                  <h3>Random Quote Machine</h3>
+                  <ul>
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>JavaScript</li>
+                  <li>React</li>
+                  <li>SCSS</li>
+                  </ul>
+               </div>
+               <div className={'project--description'}>
+                  <p>Description</p>
+                  <p>And I will bring the third part 
+                  through the fire, and will refine 
+                  them as silver is refined, and 
+                  will try them as gold is tried: 
+                  they shall call on my name, and I 
+                  will hear them: I will say, It is 
+                  my people: and they shall say, The 
+                  LORD is my God.</p>
+               </div>
+            </div>
+
+            <div className={'project--card'}>
+
+               <img src='' alt='' />
+
+               <div className={'project--specs'}>              
+                  <p>Front-End Developer</p>
+                  <h3>Random Quote Machine</h3>
+                  <ul>
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>JavaScript</li>
+                  <li>React</li>
+                  <li>SCSS</li>
+                  </ul>
+               </div>
+               <div className={'project--description'}>
+                  <p>Description</p>
+                  <p>And I will bring the third part 
+                  through the fire, and will refine 
+                  them as silver is refined, and 
+                  will try them as gold is tried: 
+                  they shall call on my name, and I 
+                  will hear them: I will say, It is 
+                  my people: and they shall say, The 
+                  LORD is my God.</p>
+               </div>
+            </div>
+
+            <div className={'project--card'}>
+
+               <img src='' alt='' />
+
+               <div className={'project--specs'}>              
+                  <p>Front-End Developer</p>
+                  <h3>Random Quote Machine</h3>
+                  <ul>
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>JavaScript</li>
+                  <li>React</li>
+                  <li>SCSS</li>
+                  </ul>
+               </div>
+               <div className={'project--description'}>
+                  <p>Description</p>
+                  <p>And I will bring the third part 
+                  through the fire, and will refine 
+                  them as silver is refined, and 
+                  will try them as gold is tried: 
+                  they shall call on my name, and I 
+                  will hear them: I will say, It is 
+                  my people: and they shall say, The 
+                  LORD is my God.</p>
+               </div>
+            </div>
+         </div>
+      </section>
+   )
+}
