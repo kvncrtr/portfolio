e1fb7332e4cf1a54c1e8a3dacbec31104140ba1e
Kevin Carter <console.logkevin@gmail.com>
2023-02-20 09:00:42 -0500

    move css nav props to nav section of file

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './components/Hero'
 import Skill from './components/Skill'
 import Languages from './components/Languages'
 import About from './components/About'
+import Projects from './components/Projects'
 
 import './styles/app.css'
 
@@ -17,95 +18,7 @@ function App() {
       <Skill />
       {/*<Languages />*/}
       <About />
-
-      <section className={'section'}>
-        <div className={'project--container'}>
-
-          <div className={'project--card'}>
-
-            <img src='' alt='' />
-
-            <div className={'project--specs'}>              
-              <p>Front-End Developer</p>
-              <h3>Random Quote Machine</h3>
-              <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>React</li>
-                <li>SCSS</li>
-              </ul>
-            </div>
-            <div className={'project--description'}>
-              <p>Description</p>
-              <p>And I will bring the third part 
-                through the fire, and will refine 
-                them as silver is refined, and 
-                will try them as gold is tried: 
-                they shall call on my name, and I 
-                will hear them: I will say, It is 
-                my people: and they shall say, The 
-                LORD is my God.</p>
-            </div>
-          </div>
-
-          <div className={'project--card'}>
-
-            <img src='' alt='' />
-
-            <div className={'project--specs'}>              
-              <p>Front-End Developer</p>
-              <h3>Random Quote Machine</h3>
-              <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>React</li>
-                <li>SCSS</li>
-              </ul>
-            </div>
-            <div className={'project--description'}>
-              <p>Description</p>
-              <p>And I will bring the third part 
-                through the fire, and will refine 
-                them as silver is refined, and 
-                will try them as gold is tried: 
-                they shall call on my name, and I 
-                will hear them: I will say, It is 
-                my people: and they shall say, The 
-                LORD is my God.</p>
-            </div>
-          </div>
-
-          <div className={'project--card'}>
-
-            <img src='' alt='' />
-
-            <div className={'project--specs'}>              
-              <p>Front-End Developer</p>
-              <h3>Random Quote Machine</h3>
-              <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>React</li>
-                <li>SCSS</li>
-              </ul>
-            </div>
-            <div className={'project--description'}>
-              <p>Description</p>
-              <p>And I will bring the third part 
-                through the fire, and will refine 
-                them as silver is refined, and 
-                will try them as gold is tried: 
-                they shall call on my name, and I 
-                will hear them: I will say, It is 
-                my people: and they shall say, The 
-                LORD is my God.</p>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Projects />
 
       <section className={'section'}>
 
