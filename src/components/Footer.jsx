a256cef2c7a0d6ae734179c5d803c76b293df82d
Kevin Carter <console.logkevin@gmail.com>
2023-02-20 20:41:53 -0500

    make footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+// import
 
 export default function Footer() {
+let email = 'console.logkevin@gmail.com'
+
    return (
-   <footer className={'section'}>
+   <footer className={'section footer--section'}>
+      <a href={email}>console.logkevin@gmail.com</a>
       <div className={'footer--icon-container'}>
       Socials Go Here
       </div>
