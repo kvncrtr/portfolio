5d01bd1fa2f7d7bb927b9f99c8f02d4a6f2ef0bb
Kevin Carter <console.logkevin@gmail.com>
2024-10-16 12:01:01 -0400

    need to push a new commit

---
+++ text.js
@@ -0,0 +1,17 @@
+const para1 = `
+I’m Kevin Carter, a versatile Software Engineer based in Atlanta, GA, with expertise in full-stack web development, specializing in React, Golang, and PostgreSQL. I have a strong track record of building scalable, responsive applications that improve user experiences and solve real-world business challenges. My passion lies in crafting innovative, efficient software solutions that make a tangible impact.
+At 100devs, I collaborated with a team to develop a project that scaled to over 25,000 users. Additionally, I spearheaded the development of three full-stack applications, improving accessibility and user engagement for over 100 users. My experience extends to quality assurance as well—while serving as a Quality Assurance Technical Lead at Material in Motion, I led a cross-functional team, optimizing server operations and improving productivity by 25%.
+One of my most recent projects, Vendex, is a full-stack inventory management system designed to help 3,000 employees manage 1.4 million parts and $100 million in assets. I enjoy taking on projects that challenge me to leverage my problem-solving abilities and bring value to organizations through technology.
+I’m a strong advocate for Agile/SCRUM methodologies and thrive in collaborative environments where I can lead, mentor, and grow with a team. I’m always seeking new opportunities to learn and share knowledge, building solutions that align with business objectives. Let’s connect!`;
+
+const para2 = `
+I’m Kevin Carter, a versatile Software Engineer based in Atlanta, GA, with expertise in full-stack web development, specializing in React, Golang, and PostgreSQL. I have a strong track record of building scalable, responsive applications that improve user experiences and solve real-world business challenges. My passion lies in crafting innovative, efficient software solutions that make a tangible impact.
+
+At 100devs, I collaborated with a team to develop a project that scaled to over 25,000 users. Additionally, I spearheaded the development of three full-stack applications, improving accessibility and user engagement for over 100 users. My experience extends to quality assurance as well—while serving as a Quality Assurance Technical Lead at Material in Motion, I led a cross-functional team, optimizing server operations and improving productivity by 25%.
+
+One of my most recent projects, Vendex, is a full-stack inventory management system designed to help 3,000 employees manage 1.4 million parts and $100 million in assets. I enjoy taking on projects that challenge me to leverage my problem-solving abilities and bring value to organizations through technology.
+
+I’m a strong advocate for Agile/SCRUM methodologies and thrive in collaborative environments where I can lead, mentor, and grow with a team. I’m always seeking new opportunities to learn and share knowledge, building solutions that align with business objectives. Let’s connect!
+`
+
+console.log(para1 === para2);
